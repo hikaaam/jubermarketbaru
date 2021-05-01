59d6453cf30d34673ee5e50a48076f3ce22d93b5
hikaaam <andrenuryana@gmail.com>
2021-05-01 15:29:44 +0700

    p

--- app/Http/Controllers/helper.php
+++ app/Http/Controllers/helper.php
@@ -301,7 +301,7 @@ class helper extends Controller
     public static function juberSyncInsert($data)
     {
         try {
-            return [$data["weigth"], $data["weight_unit"]];
+            return [$data["weight"], $data["weight_unit"]];
             if ($data["weight_unit" == "GR"]) {
                 $data["weight"] = intval($data["weight"]) / 1000;
             }
