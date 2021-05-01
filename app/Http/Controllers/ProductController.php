10bd99b57a32ee8563a0ac72a9881941af0931f6
hikaaam <andrenuryana@gmail.com>
2021-05-01 16:14:37 +0700

    test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -68,6 +68,7 @@ class ProductController extends Controller
     public function visible()
     {
         try {
+            return helper::getToken();
             $result = item::where('service', 'jbmarket')->paginate(6);
             $data["success"] = true;
             $data["code"] = 200;
