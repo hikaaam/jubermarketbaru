9610ebabf141c8017a546290d1e968961077cf6e
twenty seven <qodim.is30@gmail.com>
2021-06-18 00:44:42 +0700

    Update MerchantController.php

--- app/Http/Controllers/jbfood/MerchantController.php
+++ app/Http/Controllers/jbfood/MerchantController.php
@@ -20,10 +20,10 @@ class MerchantController extends Controller
         try {
             $merchant = Merchant::all();
             // return ResponseFormatter::success($merchant, 'Data Berhasil Diambil');
-            return helper::resp(true, 'GET', 'OKEE', $merchant)
+            return helper::resp(true, 'GET', 'OKEE', $merchant);
         } catch (\Throwable $th) {
             // return ResponseFormatter::error([], $th->getMessage(), 500);
-            return helper::resp(false, 'GET', $th->getMessage(),[],500);
+            return helper::resp(false, 'GET', $th->getMessage(), [], 500);
         }
     }
 
